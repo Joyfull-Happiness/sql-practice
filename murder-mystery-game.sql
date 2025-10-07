d699581733b72578b4deb7e8c7391c7d26e8a012
Lajoie Bradley <lajoie.bradley2@gmail.com>
2025-10-07 14:39:24 -0700

    updated part 6 and 7

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -17,8 +17,9 @@ FROM crime_scene_report
 WHERE date = '20180115' AND type='murder'AND city = 'SQL City'; 
 
 Part 2: 
---in "Northwestern Dr" and 2 witness 
--- name Annable  "Franklin Ave" 
+  -- 2 witnesses: 
+    --in "Northwestern Dr"
+    -- name Annable  "Franklin Ave" 
 SELECT * FROM person
 WHERE name LIKE '%Annabel%' AND address_street_name = 'Franklin Ave';
 
@@ -36,3 +37,37 @@ Part 4:
 SELECT * FROM get_fit_now_member WHERE name LIKE '%Annabel%';
 
 ---INFO found: id: 90081 and person_id 16371
+
+--Part 5: 
+SELECT * FROM get_fit_now_check_in WHERE membership_id = '90081';
+
+---INFO found: id: 90081 checkin date 20180109 check_in_time 16900 check_out_time
+
+--Part 6 see all ppl who were there that day : 
+SELECT * FROM get_fit_now_check_in 
+WHERE check_in_date = '20180109' AND check_in_time = '1600' OR check_out_time = '1700' 
+
+-- info found: members who were there: 
+-- membership_id	check_in_date	check_in_time	check_out_time
+--48Z7A	20180109	1600	1730
+--48Z55	20180109	1530	1700
+--90081	20180109	1600	1700 <Annabel>
+
+Par 7: 
+--Part 7 see all ppl who were there that day : 
+SELECT * FROM get_fit_now_member
+WHERE id = '48Z7A' OR id = '48Z55'; 
+
+-- info found: 
+--id	person_id	name	membership_start_date	membership_status
+--48Z55	67318	Jeremy Bowers	20160101	gold
+--48Z7A	28819	Joe Germuska	20160305	gold
+
+
+
+
+
+
+
+
+
