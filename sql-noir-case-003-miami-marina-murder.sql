842fadd64d155a9d8500d3f787a849ed873b5bd1
Lajoie Bradley <lajoie.bradley2@gmail.com>
2025-10-15 14:16:23 -0700

    done

--- sql-noir-case-003-miami-marina-murder.sql
+++ sql-noir-case-003-miami-marina-murder.sql
@@ -47,7 +47,28 @@ WHERE person.name LIKE '%ez' OR  person.name LIKE 'ul%';
 -- Witnesses 1: 101	Carlos Mendez	Los Ojos	Fisherman	369 Ocean Drive	101	101	I saw someone check into a hotel on August 13. The guy looked nervous.
 -- Witnesses 2: 102	Raul Gutierrez	The Cobra	Nightclub Owner	45 Sunset Ave	102	58	NULL
 
---3) looking at hotel chekck in 
+--3) looking at interviews since Raul's is null
+SELECT * FROM interviews WHERE person_id = 101 OR person_id = 102;
+
+--notes: I heard someone checked into a hotel with "Sunset" in the name.
+
+--4) cross surveillance and hotel checkincs
+
+SELECT * FROM surveillance_records INNER JOIN hotel_checkins ON surveillance_records.hotel_checkin_id = hotel_checkins.id
+WHERE hotel_checkins.check_in_date = 19860813 AND hotel_checkins.hotel_name LIKE '%Sunset%';
+-- suspicious activity: 8	8	2	Left suddenly at 3 AM	2	27	Sunset Bay Hotel	19860813
+
+-- person id  (surveillance)= 8
+--person id (hotel_checkin_ = 8
+
+
+--5 
+SELECT * FROM person INNER JOIN confessions ON person.id = confessions.person_id 
+WHERE person.id = '8' ;
+
+8	Thomas Brown	The Fox	Dock Worker	234 Port Street	73	8	Alright! I did it. I was paid to make sure he never left the marina alive.
+
+
 
 
 
