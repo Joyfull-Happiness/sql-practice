6c42fe924ac0bd7aa726b5a315d7d29f9111181e
Lajoie Bradley <lajoie.bradley2@gmail.com>
2025-10-15 12:35:06 -0700

    Solved

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -2,3 +2,9 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+SELECT * 
+FROM interviews 
+JOIN suspects ON interviews.suspect_id = suspects.id 
+WHERE transcript IS NOT NULL 
+AND scar = 'left cheek' 
+AND attire = 'trench coat';
