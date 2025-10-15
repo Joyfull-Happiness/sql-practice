84906fcc41a176a6359e4a8b21abd234af3aec3b
Lajoie Bradley <lajoie.bradley2@gmail.com>
2025-10-15 12:35:34 -0700

    answer
    Added query result for suspect Vincent Malone.

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -8,3 +8,7 @@ JOIN suspects ON interviews.suspect_id = suspects.id
 WHERE transcript IS NOT NULL 
 AND scar = 'left cheek' 
 AND attire = 'trench coat';
+
+Answer: 
+suspect_id	transcript	id	name	attire	scar
+183	I wasn’t going to steal it, but I did.	183	Vincent Malone	trench coat	left cheek
