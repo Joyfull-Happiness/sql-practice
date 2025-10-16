7b7f8675db05f4a41879f1d3d9ef7db819961999
Lajoie Bradley <lajoie.bradley2@gmail.com>
2025-10-16 10:30:01 -0700

    Update SQL query for witness and guest filtering
    Refine SQL query to filter consultant by name ending with 'an'.

--- sql-noir-case-004-vanishing-diamond.sql
+++ sql-noir-case-004-vanishing-diamond.sql
@@ -16,7 +16,8 @@ WHERE location LIKE '%Fontainebleau Hotel%';
 --id	date	location	description
 --48	19870520	Fontainebleau Hotel	The Heart of Atlantis necklace disappeared. 
 --Many guests were questioned but only two of them gave valuable clues. 
---One of them is a really famous actor. The other one is a woman who works as a consultant for a big company and her first name ends with "an".
+--One of them is a really famous actor. 
+--The other one is a woman who works as a consultant for a big company and her first name ends with "an".
 
 
 -- famous actor 
@@ -30,15 +31,18 @@ FROM witness_statements
 WHERE id ='48';
 
 --48	78	From the parking lot, I saw someone copying down the security patrol schedule
-
-  2
   
 
 --3) 
 SELECT *
 FROM guest INNER JOIN final_interviews
 ON guest.id = final_interviews.id
-WHERE occupation LIKE '%actor%' OR occupation LIKE '%consultant%';
+WHERE occupation LIKE '%actor%' OR (occupation LIKE '%consultant%' AND name LIKE '%an%');
+id                                  guest_id
+-- 129	Clint Eastwood	Actor	VIP-G	129	129	I was taking care of my sick mother. I would never commit murder.
+
+-- id	name	occupation	invitation_code	id	guest_id	confession
+-- 116	Vivian Nair	Consultant	VIP-R	116	116	Check my work computer logs. I would never commit such a horrible act.
 
 
   --4) 
